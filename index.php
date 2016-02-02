a8880c251e761e9afcfc545f7d396cb52271bacd
SimpleAOB <simpleaob@live.com>
2016-02-02 12:07:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,25 +7,38 @@
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <script>
-$(document).ready(function(){
-	$('#main_content').load('budget_overview.php');
-	
-	$('.navbar_link').on('click', function(){
-		var link = $(this).attr('href');
-		$('#main_content').load(link);
-		return false;
-	})
-})
+    var jqSupported = false;
+    $(document).ready(function(){
+        $('#main_content').load('budget_overview.php');
+
+        $('.navbar_link').on('click', function(){
+            var link = $(this).attr('href');
+            $('#main_content').load(link);
+            return false;
+        })
+    });
+
+    window.onload = function() {
+        pageInit();
+    }
+
+    function pageInit() {
+        if (typeof jQuery == 'undefined') {
+            alert("Your device does not support jQuery (or some other error)");
+        } else {
+            jqSupported = true;
+        }
+    }
 
 </script>
 <body>
 <header>
-	<div id="logo"><h1>Budgeting App</h1></div>
-	<div id="navbar">
-		<a href="cardmanager.php" id="card_manager" class="navbar_link">Credit Card Manager</a>
-		<a href="budget_overview.php" id="budget_overview" class="navbar_link">Budget Overview</a>
-		
-	</div>
+    <div id="logo"><h1>Budgeting App</h1></div>
+    <div id="navbar">
+        <a href="cardmanager.php" id="card_manager" class="navbar_link">Credit Card Manager</a>
+        <a href="budget_overview.php" id="budget_overview" class="navbar_link">Budget Overview</a>
+
+    </div>
 </header>
 <section id="main_content">
 
